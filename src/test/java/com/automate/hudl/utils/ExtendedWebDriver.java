9bc013355c989b811f9dc686fa3dc95279b87610
Geethanjali.Raja <geethanjali.raja@adstream.com>
2022-11-30 01:05:10 +0000

    Revert "Test automation for hudl website login feature"
    This reverts commit dc799e7d6c658212c24d3f2b81de9c2c3d526d3c.

--- src/test/java/com/automate/hudl/utils/ExtendedWebDriver.java
+++ src/test/java/com/automate/hudl/utils/ExtendedWebDriver.java
@@ -15,50 +15,53 @@ public class ExtendedWebDriver {
 
     public WebDriver getDriver() throws IOException {
 
-        String url = getProp().getProperty("baseUrl");
-        String browser_properties = getProp().getProperty("browser");
-        String browser_maven = System.getProperty("browser");
+        String url = getProp().getProperty("ProdUrl");
+        String browser_properties = getProp().getProperty("Browser");
+        String platform_properties = getProp().getProperty("Platform");
+        String platform_maven = getProp().getProperty("Platform");
+        String browser_maven = System.getProperty("Browser");
 
         String browser = browser_maven != null ? browser_maven : browser_properties;
-
+        String platform = platform_maven != null ? platform_maven : platform_properties;
+        String driverPath;
         if (driver == null) {
-            String os = System.getProperty("os.name").toLowerCase().substring(0, 3);
+
             if (browser.equalsIgnoreCase("chrome")) {
-                switch (os) {
-                    case "win":
+                switch (platform) {
+                    case "windows":
                         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//test//resources//webDrivers//chromedriver-win.exe");
                         break;
-                    case "lin":
-                        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//test//resources//webDrivers//chromedriver-lnx");
+                    case "linux":
+                        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//test//resources//webDrivers//chromedriver-linux");
                         break;
                     case "mac":
-                        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//test//resources//webDrivers//chromedriver-mac64");
+                        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//test//resources//webDrivers//chromedriver-mac");
                         break;
                 }
                 driver = new ChromeDriver();
             }
             if (browser.equalsIgnoreCase("firefox")) {
-                switch (os) {
-                    case "win":
+                switch (platform) {
+                    case "windows":
                         System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//src//test//resources//webDrivers//geckodriver-win.exe");
                         break;
-                    case "lin":
-                        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//src//test//resources//webDrivers//geckodriver-lnx");
+                    case "linux":
+                        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//src//test//resources//webDrivers//geckodriver-linux");
                         break;
                     case "mac":
-                        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//src//test//resources//webDrivers//geckodriver-mac64");
+                        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//src//test//resources//webDrivers//geckodriver-mac");
                         break;
                 }
                 driver = new FirefoxDriver();
             }
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.get(url);
         }
         return driver;
     }
 
     public Properties getProp() throws IOException {
-        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//test//resources//testConfig.properties");
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//main//resources//properties//hudl_prod.properties");
         Properties prop = new Properties();
         prop.load(fis);
         return prop;
